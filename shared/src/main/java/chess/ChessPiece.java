286b4ae76fae55d621cc2e456456118aa70b582f
aaj54 <aaj54@byu.edu>
2026-04-30 15:20:06 -0600

    impl king

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -71,7 +71,7 @@ public class ChessPiece {
                 break;
 
             case KING:
-                //calculator = new KingMoves();
+                calculator = new KingMoves();
                 break;
 
             case KNIGHT:
--- shared/src/main/java/chess/KingMoves.java
+++ shared/src/main/java/chess/KingMoves.java
@@ -1,4 +1,50 @@
 package chess;
 
-public class KingMoves {
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class KingMoves implements PieceMovesCalculator{
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition)
+    {
+        //Set an array of where the queen can move
+        Collection<ChessMove> moves = new ArrayList<>();
+
+        //get necessary piece values and locations
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        ChessPiece piece = board.getPiece(myPosition);
+        ChessGame.TeamColor currentPlayerColor = piece.getTeamColor();
+
+        //Set directions that KING can move in array (diagonal and in straight line 1 space)
+        int[][] kingDir = {{-1,1}, {0,1}, {1,1}, {-1,0}, {1,0}, {-1,-1}, {0,-1}, {1,-1}};
+
+        //For loop to loop though movement options
+        for (int[] dir : kingDir)
+        {
+            int r = row + dir[0];
+            int c = col + dir[1];
+
+            //set new position
+            ChessPosition newPosition = new ChessPosition(r,c);
+            ChessPiece newSpot = board.getPiece(newPosition);
+
+            //check for empty spot or opposite pawn
+            if(newSpot == null)
+            {
+                //add new location and piece to moves
+                moves.add(new ChessMove(myPosition, newPosition, null));
+            }
+            //Enemy in the way
+            else
+            {
+                if(newSpot.getTeamColor() != currentPlayerColor)
+                {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                }
+                break;
+            }
+        }
+        return moves;
+    }
 }
