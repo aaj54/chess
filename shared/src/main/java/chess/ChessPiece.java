11bd6d17be40edfcaf733ad9fb939dc27835805d
aaj54 <aaj54@byu.edu>
2026-05-03 19:42:04 -0700

    add hashcode and equals to piece and board

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -86,5 +87,19 @@ public class ChessPiece {
                 throw new RuntimeException("Unknown piece type");
         }
         return calculator.pieceMoves(board, myPosition);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece piece = (ChessPiece) o;
+        return pieceColor == piece.pieceColor && type == piece.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 }
