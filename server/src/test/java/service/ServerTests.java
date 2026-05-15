3800cd4295582e1f512c34d3392b2a859465020a
aaj54 <aaj54@byu.edu>
2026-05-14 22:54:15 -0700

    start clear test and get working

--- server/src/test/java/service/ServerTests.java
+++ server/src/test/java/service/ServerTests.java
@@ -3,20 +3,30 @@ package service;
 import dataaccess.DataAccessException;
 import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryUserDAO;
+import dataaccess.MemoryGameDAO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ServerTests {
-    // reset DAOs before each test so they don't bleed into each other
     private UserSer userService;
+    private ClearSer clearService;
 
     @BeforeEach
     void setup() {
         MemoryUserDAO userDAO = new MemoryUserDAO();
         MemoryAuthDAO authDAO = new MemoryAuthDAO();
+        MemoryGameDAO gameDAO = new MemoryGameDAO();
         userService = new UserSer(userDAO, authDAO);
+        clearService = new ClearSer(userDAO, authDAO, gameDAO);
+    }
+
+    @Test
+    void clearSuccess() throws Exception {
+        userService.register(new RegRequest("user", "pass", "e@mail.com"));
+        clearService.clear();
+        assertDoesNotThrow(() -> userService.register(new RegRequest("user", "pass", "e@mail.com")));
     }
 
     @Test
