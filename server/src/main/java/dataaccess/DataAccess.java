305d6388d743fd25d52091b61436cc3bfeb96487
aaj54 <aaj54@byu.edu>
2026-05-17 22:16:50 -0700

    make dataAccess interface

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,29 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+
+public interface DataAccess {
+    //From MemUser
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+    void clear() throws DataAccessException;
+
+    //From MemAuth
+    void createAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+
+    //From MemGame
+    int createGame(String gameName) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+
+}
+
+
+
