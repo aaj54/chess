0c86664efc83ddada9c4169dfe8f4b99fc29c3ca
aaj54 <aaj54@byu.edu>
2026-05-25 22:37:52 -0700

    add register and login

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -4,6 +4,8 @@ import model.*;
 import org.junit.jupiter.api.*;
 import server.Server;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class ServerFacadeTests {
 
@@ -28,6 +30,41 @@ public class ServerFacadeTests {
     public void clear() throws Exception
     {
         facade.clear();
+    }
+
+    @Test
+    void regSuccess() throws Exception
+    {
+        AuthData auth = facade.register("user", "pass", "e@mail.com");
+        assertNotNull(auth.authToken());
+        assertEquals("user", auth.username());
+    }
+
+    @Test
+    void regFail() throws Exception
+    {
+        facade.clear();
+        facade.register("user", "pass", "e@mail.com");
+        assertThrows(Exception.class, () ->
+                facade.register("user", "pass", "e@mail.com"));
+    }
+
+    @Test
+    void loginSuccess() throws Exception
+    {
+        facade.register("user", "pass", "e@mail.com");
+        AuthData auth = facade.login("user", "pass");
+        assertNotNull(auth.authToken());
+        assertEquals("user", auth.username());
+    }
+
+    @Test
+    void loginFail() throws Exception
+    {
+        facade.clear();
+        facade.register("user", "pass", "e@mail.com");
+        assertThrows(Exception.class, ()->
+                facade.login("user", "wrongpass"));
     }
 
 }
