b00ba0e48230c688a2857aaec8dc2171272fcd5f
aaj54 <aaj54@byu.edu>
2026-06-06 17:54:08 -0700

    work even more on websocket

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -39,8 +39,8 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
             switch (command.getCommandType()) {
                 case CONNECT -> connect(command, ctx.session);
                 case MAKE_MOVE -> makeMove(ctx.session, gson.fromJson(ctx.message(), MakeMove.class));
-                case LEAVE -> leave(ctx.session, command);
-                case RESIGN -> resign(ctx.session, command);
+                case LEAVE -> leave(command, ctx.session);
+                case RESIGN -> resign(command, ctx.session);
             }
         } catch (Exception ex) {
             try {
@@ -85,20 +85,107 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         }
     }
 
-    private void leave(String visitorName, Session session) throws IOException {
-        var message = String.format("%s left the shop", visitorName);
-        var notification = new Notification(Notification.Type.DEPARTURE, message);
-        connections.broadcast(session, notification);
-        connections.remove(session);
+    private void leave(UserGameCommand command, Session session) throws IOException {
+        try {
+            AuthData auth = dataAccess.getAuth(command.getAuthToken());
+            if (auth == null) {
+                connections.sendToSession(session, new ErrorMessage("Error: unauthorized"));
+                return;
+            }
+            GameData game = dataAccess.getGame(command.getGameID());
+            if (game == null) {
+                connections.sendToSession(session, new ErrorMessage("Error: game not found"));
+                return;
+            }
+            String username = auth.username();
+            if (username.equals(game.whiteUsername())) {
+                dataAccess.updateGame(new GameData(game.gameID(), null,
+                        game.blackUsername(), game.gameName(), game.game()));
+            } else if (username.equals(game.blackUsername())) {
+                dataAccess.updateGame(new GameData(game.gameID(), game.whiteUsername(),
+                        null, game.gameName(), game.game()));
+            }
+            connections.remove(command.getGameID(), session);
+            connections.broadcast(command.getGameID(), session,
+                    new NotifyMessage(username + " left the game"));
+        } catch (DataAccessException e) {
+            connections.sendToSession(session, new ErrorMessage("Error: " + e.getMessage()));
+        }
     }
 
-    public void makeMove(String petName, String sound) throws ResponseException {
+    public void makeMove(Session session, MakeMove command) throws IOException {
+        //copy from connect
         try {
-            var message = String.format("%s says %s", petName, sound);
-            var notification = new Notification(Notification.Type.NOISE, message);
-            connections.broadcast(null, notification);
-        } catch (Exception ex) {
-            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+            AuthData auth = dataAccess.getAuth(command.getAuthToken());
+            if (auth == null) {
+                connections.sendToSession(session, new ErrorMessage("Error: unauthorized"));
+                return;
+            }
+            GameData gameData = dataAccess.getGame(command.getGameID());
+            if (gameData == null) {
+                connections.sendToSession(session, new ErrorMessage("Error: game not found"));
+                return;
+            }
+            ChessGame game = gameData.game();
+            String username = auth.username();
+
+            if (game.isGameOver()) {
+                connections.sendToSession(session, new ErrorMessage("Error: game is over"));
+                return;
+            }
+
+            ChessGame.TeamColor color = null;
+            if (username.equals(gameData.whiteUsername())) {
+                color = ChessGame.TeamColor.WHITE;
+            }
+            else if (username.equals(gameData.blackUsername())) {
+                color = ChessGame.TeamColor.BLACK;
+            }
+            if (color == null) {
+                connections.sendToSession(session, new ErrorMessage("Error: observers cannot make moves"));
+                return;
+            }
+            if (game.getTeamTurn() != color) {
+                connections.sendToSession(session, new ErrorMessage("Error: not your turn"));
+                return;
+            }
+            try {
+                game.makeMove(command.getMove());
+            } catch (Exception e) {
+                connections.sendToSession(session, new ErrorMessage("Error: invalid move"));
+                return;
+            }
+
+            GameData updGame = new GameData(gameData.gameID(), gameData.whiteUsername(),
+                    gameData.blackUsername(), gameData.gameName(), game);
+            dataAccess.updateGame(updGame);
+
+            connections.broadcast(command.getGameID(), null, new LoadMessage(updGame));
+            connections.broadcast(command.getGameID(), session,
+                    new NotifyMessage(username + " made move: " + command.getMove()));
+
+            ChessGame.TeamColor opponent = color == ChessGame.TeamColor.WHITE ?
+                    ChessGame.TeamColor.BLACK : ChessGame.TeamColor.WHITE;
+            String opponentName = color == ChessGame.TeamColor.WHITE ?
+                    gameData.blackUsername() : gameData.whiteUsername();
+
+            if (game.isInCheckmate(opponent)) {
+                game.setGameOver(true);
+                dataAccess.updateGame(updGame);
+                connections.broadcast(command.getGameID(), null,
+                        new NotifyMessage(opponentName + " is in checkmate! Game over."));
+            }
+            else if (game.isInStalemate(opponent)) {
+                game.setGameOver(true);
+                dataAccess.updateGame(updGame);
+                connections.broadcast(command.getGameID(), null,
+                        new NotifyMessage("Stalemate! Game over."));
+            } else if (game.isInCheck(opponent)) {
+                connections.broadcast(command.getGameID(), null,
+                        new NotifyMessage(opponentName + " is in check!"));
+            }
+        } catch (DataAccessException e) {
+            connections.sendToSession(session, new ErrorMessage("Error: " + e.getMessage()));
         }
     }
 
